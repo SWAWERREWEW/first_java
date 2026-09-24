387d846677965c5b027b44fe43a10277e8ceee14
artkor <artkor05@gmail.com>
2026-09-24 13:36:06 +0500

    rows with symbol a

--- src/Main.java
+++ src/Main.java
@@ -46,6 +46,8 @@ String непуст = "  ";
 if(input_txt[i].intern() != ""){непуст = input_txt[i];}
 if(непуст.substring(0, 2).equalsIgnoreCase("ra") & непуст.substring(0, 2).intern() == "ra")
 {pri.nt(input_txt[i]);}}
+
+
 double y0 = 1.31522; double y1 = 1.31230; double y2 = 1.32730; double y3 = 1.33353; double h = 0.002;
 double fx2 = 1/(6*h) * (y0 - 6*y1 + 3*y2 + 2*y3);
 double fx3 = 1/(6*h) * (-2*y0 + 9*y1 - 18*y2 + 11*y3);
@@ -56,4 +58,39 @@ double fx1 = 1/(  6*up(h, 2)  ) * ( 6*y0 - 12*y1 + 6*y2);
 fx2 =        1/(  6*up(h, 2)  ) * ( 6*y1 - 12*y2 + 6*y3);
 fx3 =        1/(  6*up(h, 2)  ) * (-6*y0 + 24*y1 - 30*y2 + 12*y3);
 pri.nt("О господь🌌🔥"); pri.nt(fx0); pri.nt(fx1); pri.nt(fx2); pri.nt(fx3);
+
+String[] lil_values = new String[999];
+int[] lil_lengths = new int[999];
+int length_data = 0;
+
+pri.nt("------- Строки с буквой a -----");
+for(int i=1; i<input_txt.length & input_txt[i] != null; i++){
+lil_values[i] = input_txt[i];
+lil_lengths[i] = input_txt[i].length();
+length_data++;}
+
+//for(int i=1; i<length_data; i++){
+//// pri.n(lil_values[i] + " &="); pri.n(lil_lengths[i]); pri.nt("");
+//for(int ii=0; ii<lil_lengths[i]; ii++){
+//// if(lil_values[i].substring(ii, ii+1).intern() == "a"){pri.nt(lil_values[ii] +" Строка с буквой a найдена✅");}
+//pri.n(ii); pri.n(ii+1); pri.nt(" " + lil_values[i].substring(ii, ii+1));
+//if(lil_values[i].substring(ii, ii+1).equalsIgnoreCase("a")){pri.nt("Буква a ✅✅✅☕");}  }
+//pri.nt("next row");}
+
+for(int i=1; i<length_data+1; i++){
+String need_row = "";
+for(int ii=0; ii<lil_lengths[i]; ii++){
+if((lil_values[i]).substring(ii, ii+1).equalsIgnoreCase("a"))
+{need_row = lil_values[i];}  }
+if(!need_row.equalsIgnoreCase("")){pri.nt(need_row);}  }
+
+//Map<String, Integer> словарь = new HashMap<>();
+//словарь.put("ключ", 42); // Отсылка к произведению Дугласа Адамса «Автостопом по галактике»
+//Integer значение = словарь.get("ключ");
+//pri.nt(значение); pri.nt(словарь);
+// // with error
+// // java: cannot find symbol
+// // symbol:   class Map
+// // location: class Main
+
 }}
